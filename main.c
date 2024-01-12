ea69555825943c4da405b2c1f3adac26b8f884d6
Kirill Gusev <myaumuraa@gmail.com>
2024-01-12 18:42:54 +0300

    Finished pow and fact

--- main.c
+++ main.c
@@ -10,14 +10,18 @@
 #define FAC '!'
 
 typedef struct {
-    short int digits[MAX_DIGITS];
+    int digits[MAX_DIGITS];
     short int length;
 } bn;
 
+// MARK: - Expression
+
+void read_expression(void);
+
 // MARK: - Init
 
-void create_bn(bn*, char *);            // success
-void init_bn(bn*);
+void create_bn(bn*, char*);             // success
+void init_bn(bn*);                      // success
 void print_bn(bn*);                     // success
 void read_expression(void);
 
@@ -27,51 +31,48 @@ void add_bn(bn*, bn*, bn*);             // success
 void sub_bn(bn*, bn*, bn*);             // success
 void mul_bn(bn*, bn*, bn*);             // success
 
-void fact_bn(bn*, bn*);
-void pow_bn(bn*, bn*, short int);       // success
+void fact_bn(bn*, bn*);                 // success
+void pow_bn(bn*, bn*, bn*);             // success
 void sum_bn_from_to(bn*, bn*, bn*);
 
 // MARK: - Support
 
-int bn_is_zero(bn*);
-void bn_dec(bn*);
+int  bn_is_zero(bn*);                   // success
+void bn_dec(bn*);                       // success
+void bn_inc(bn*);                       // success
+int  bn_cmp(bn*, bn*);                  // success
 void clear_nulls(bn*);                  // success
-void copy_bn(bn*, bn*);
+void copy_bn(bn*, bn*);                 // success
 
 // MARK: - Main
 
 int main(void) {
     
-    char str1[MAX_DIGITS] = "10", str2[MAX_DIGITS] = "3131";
+    char str1[MAX_DIGITS] = "2", str2[MAX_DIGITS] = "100";
     bn num1, num2, result;
     
     create_bn(&num1, str1);
     create_bn(&num2, str2);
     init_bn(&result);
-//    mul_bn(&result, &num1, &num2);
-    pow_bn(&result, &num1, 0);
-//    bn_dec(&num1);
-//    copy_bn(&num1, &result);
 //    fact_bn(&result, &num1);
+//    pow_bn(&result, &num1, &num2);
+//    bn_dec(&num1);
     print_bn(&result);
     return EXIT_SUCCESS;
 }
 
 // MARK: - Инициализация большого числа
 
-void init_bn(bn * bn) {
-//    memset(bn->digits, 0, sizeof(bn->digits) * MAX_DIGITS);
-    for (int i = 0; i < MAX_DIGITS; i++) {
-        bn->digits[i] = 0;
-    }
-    bn->length = 2500;
+void init_bn(bn * num) {
+    memset(num->digits, 0x00, sizeof(int) * MAX_DIGITS);
+    num->length = 2500;
 }
 
 // MARK: - Создание большого числа
 
 void create_bn(bn *num, char *str) {
     num->length = strlen(str);
-    memset(num->digits, 0x00, sizeof(short int) * MAX_DIGITS);
+    memset(num->digits, 0x00, sizeof(int) * MAX_DIGITS);
     for (short int i = 0; i < num->length; i++) num->digits[num->length - i - 1] = str[i] - '0';
 }
 
@@ -141,15 +142,15 @@ void mul_bn(bn * result, bn * first_number, bn * second_num) {
 
 // MARK: - Факториал большого числа
 
-void fact_bn(bn* result, bn* number) {
+void fact_bn(bn* result, bn* num) {
     
     bn temp;
-    copy_bn(number, &temp);
-    bn_dec(number);
+    copy_bn(num, &temp);
+    bn_dec(num);
     
-    while (!bn_is_zero(number)) {
-        mul_bn(result, &temp, number);
-        bn_dec(number);
+    while (!bn_is_zero(num)) {
+        mul_bn(result, &temp, num);
+        bn_dec(num);
         copy_bn(result, &temp);
         init_bn(result);
     }
@@ -158,19 +159,20 @@ void fact_bn(bn* result, bn* number) {
 
 // MARK: - Возведение в степень большого числа
 
-void pow_bn(bn* result, bn* number, short int exp) {
+void pow_bn(bn* result, bn* num, bn* exp) {
     
-    if (exp == 0) {
+    if (bn_is_zero(exp)) {
         result->digits[0] = 1;
         result->length = 1;
     } else {
         bn temp;
         init_bn(&temp);
-        copy_bn(number, &temp);
+        copy_bn(num, &temp);
+        bn_dec(exp);
         
-        for (int i = 0; i < exp; i++) {
-            mul_bn(result, &temp, number);
-            exp--;
+        while (!bn_is_zero(exp)) {
+            mul_bn(result, &temp, num);
+            bn_dec(exp);
             copy_bn(result, &temp);
             init_bn(result);
         }
@@ -178,7 +180,7 @@ void pow_bn(bn* result, bn* number, short int exp) {
     }
 }
 
-// MARK: - Сумма всех чисел от большого числа до большого числа
+// MARK: - Сумма всех чисел от и до
 
 void sum_bn_from_to(bn* result, bn* first_num, bn* second_num) {
     
@@ -186,43 +188,84 @@ void sum_bn_from_to(bn* result, bn* first_num, bn* second_num) {
 
 // MARK: - Проверка числа на ноль
 
-int bn_is_zero(bn* n) {
-
-    for (int i = 0; i < n->length; i++) {
-    if (n->digits[i]) {
-      return 0;
+int bn_is_zero(bn* num) {
+    for (int i = 0; i < num->length; i++) {
+        if (num->digits[i]) {
+            return 0;
+        }
     }
-  }
-  return 1;
+    return 1;
 }
 
 // MARK: - Уменьшение числа на единицу
 
 void bn_dec(bn *num) {
-      for (int i = 0; i < num->length; i++) {
-        int tmp = num->digits[i];
-        int res = tmp - 1;
-          num->digits[i] = res;
+    short int carry_flag = 1;
 
-        if (!(res > tmp))
-        {
-          break;
+    for (short int i = 0; i < num->length; i++) {
+        short int sub = 0;
+        
+        sub = num->digits[i] - carry_flag;
+        
+        if (sub < 0) {
+            sub += 10;
+            carry_flag = 1;
+        } else {
+            carry_flag = 0;
         }
-      }
+        num->digits[i] = sub;
+    }
+    clear_nulls(num);
+}
+
+// MARK: - Увеличение числа на единицу
+
+void bn_inc(bn* num) {
+    int res, temp;
+    
+    for (int i = 0; i < num->length; i++) {
+        temp = num->digits[i];
+        res = temp + 1;
+        num->digits[i] = res;
+
+        if (res > temp) break;
+    }
+}
+
+// MARK: - Сравнение больлих чисел
+
+int bn_cmp(bn* first_num, bn* second_num) {
+
+  int i = MAX_DIGITS;
+  do {
+    i -= 1;
+    if (first_num->digits[i] > second_num->digits[i]) {
+      return 1;
+    } else if (first_num->digits[i] < second_num->digits[i]) {
+      return -1;
+    }
+  } while (i != 0);
+
+  return 0;
 }
 
 // MARK: - Удаление нулей перед числом
 
-void clear_nulls(bn * big_num) {
-    while (big_num->digits[big_num->length - 1] == 0 && big_num->length > 1) big_num->length--;
+void clear_nulls(bn* num) {
+    while (num->digits[num->length - 1] == 0 && num->length > 1) num->length--;
 }
 
 // MARK: - Функция копирования структуры
 
-void copy_bn(bn* number, bn* copy) {
-    for(int i = 0; i < number->length; i++) {
-        copy->digits[i] = number->digits[i];
+void copy_bn(bn* src, bn* dst) {
+    for(int i = 0; i < src->length; i++) {
+        dst->digits[i] = src->digits[i];
     }
-    copy->length = number->length;
+    dst->length = src->length;
 }
 
+// MARK: - Чтение выражения
+
+void read_expression(void) {
+    
+}
